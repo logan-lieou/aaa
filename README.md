5ee464dfedad100917f6cf686be0285b34b9e618
Logan <logan.lieou@protonmail.com>
2021-09-03 02:33:33 -0500

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+## Computer Vision Project
+
+Yeah well I want to learn CV real quick so what do I want to make?
+Object detection? Some sort of detection right this seems to be the only thing it's good at.
---
+++ build/CMakeFiles/computer_vision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computer_vision.dir/src/main.cpp.o"
+  "CMakeFiles/computer_vision.dir/src/main.cpp.o.d"
+  "computer_vision"
+  "computer_vision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computer_vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
